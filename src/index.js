bea2edb836dcfbc099fef0f2611a8bf40a490636
mspirovv <mspirov@abv.bg>
2024-09-19 21:33:58 +0300

    Render first view

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log("works");
+import express from 'express';
+import handlebars from 'express-handlebars'
+
+const app = express();
+
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs',
+}))
+app.set('view engine', 'hbs');
+app.set('views', './src/views');
+
+app.get('/' , (req,res) => {
+    res.render('index');
+})
+
+app.listen(5000, () => console.log("Server is running on localhost:5000"))
