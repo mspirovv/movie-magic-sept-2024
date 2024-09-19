3697b247fe81071822044b9034cf905b5781469a
mspirovv <mspirov@abv.bg>
2024-09-19 22:08:47 +0300

    Add home controller

---
+++ src/controllers/homeController.js
@@ -0,0 +1,14 @@
+import {Router} from 'express';
+
+const router= Router();
+
+router.get('/', (req,res) => {
+    res.render('home');
+});
+
+router.get('/test', (req,res) => {
+    res.send('test');
+});
+
+
+export default router
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import express from 'express';
-import handlebars from 'express-handlebars'
+import handlebars from 'express-handlebars';
+
+import homeController from './controllers/homeController.js'
 
 const app = express();
 
@@ -11,8 +13,6 @@ app.set('views', './src/views');
 
 app.use(express.static('public'));
 
-app.get('/' , (req,res) => {
-    res.render('home');
-})
+app.use(homeController);
 
 app.listen(5000, () => console.log("Server is running on localhost:5000"))
