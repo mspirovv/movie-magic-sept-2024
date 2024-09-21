4f70174eb065c3076a6227c12e06f8a2c53cfec6
mspirovv <mspirov@abv.bg>
2024-09-21 11:16:06 +0300

    Add create movie page

---
+++ src/controllers/movieController.js
@@ -0,0 +1,11 @@
+import { Router } from "express";
+
+const router = Router();
+
+//URL /movies/create
+router.get('/create', (req,res) => {
+    res.render('movies/create')
+
+});
+
+export default router;
--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,9 @@ import { Router } from "express";
 const router = Router();
 
 import homeController from './controllers/homeController.js';
+import movieController from './controllers/movieController.js';
 
 router.use(homeController);
+router.use('/movies', movieController)
 
 export default router;
