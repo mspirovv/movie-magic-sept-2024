b99f3d32d6757158c57d8372485561001d1a7c86
mspirovv <mspirov@abv.bg>
2024-09-21 13:11:29 +0300

    Add movie rating

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,10 @@ import homeController from './controllers/homeController.js';
 import movieController from './controllers/movieController.js';
 
 router.use(homeController);
-router.use('/movies', movieController)
+router.use('/movies', movieController);
+router.all('*', (req, res) => {
+    res.render('404')
+});
+
 
 export default router;
