98eaeb89a72944f370710ee8fb0bb5a7b9f24a23
mspirovv <mspirov@abv.bg>
2024-09-27 21:01:10 +0300

    add db connection

---
+++ src/config/handlebarsInit.js
@@ -0,0 +1,10 @@
+import handlebars from 'express-handlebars';
+
+export default function handlebarsInit(app) {
+    app.engine('hbs', handlebars.engine({
+        extname: 'hbs',
+    }))
+    app.set('view engine', 'hbs');
+    app.set('views', './src/views');
+
+};
