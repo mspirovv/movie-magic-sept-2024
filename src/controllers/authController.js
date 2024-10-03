9046a9d0851c2706db435fecdd60fb5fcd54b0c2
mspirovv <mspirov@abv.bg>
2024-10-03 21:32:27 +0300

    Add register page

---
+++ src/controllers/authController.js
@@ -0,0 +1,10 @@
+import { Router } from "express"
+
+const router = Router();
+
+router.get('/register', (req,res) => {
+
+    res.render('auth/register')
+});
+
+export default router;
--- src/routes.js
+++ src/routes.js
@@ -5,10 +5,12 @@ const router = Router();
 import homeController from './controllers/homeController.js';
 import movieController from './controllers/movieController.js';
 import castController from './controllers/castController.js'
+import authController from './controllers/authController.js'
 
 router.use(homeController);
 router.use('/movies', movieController);
 router.use('/cast', castController);
+router.use('/auth',authController)
 router.all('*', (req, res) => {
     res.render('404')
 });
