f02aeea66a1977f36b5176be7140246a17fe4642
mspirovv <mspirov@abv.bg>
2024-10-03 22:18:06 +0300

    Validate user and password

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -22,4 +22,12 @@ router.get('/login', (req,res) => {
     res.render('auth/login');
 })
 
+router.post('/login', async (req,res) => {
+    const { email , password } = req.body;
+
+    await authService.login(email,password);
+
+    res.redirect('/');
+
+})
 export default router;
--- src/services/authService.js
+++ src/services/authService.js
@@ -1,10 +1,28 @@
-import User from "../models/User.js"
+import User from "../models/User.js";
+import bcrypt from 'bcrypt';
 
 const register = (email,password) => {
     return User.create({ email,password })
 
 }
 
+const login = async (email,password) => {
+    const user = await User.findOne({ email }) ;
+
+    if (!user) {
+        throw new Error('User does not exist!');
+    }
+
+    const isValid = await bcrypt.compare(password,user.password);
+
+    if (!isValid) {
+        throw new Error('Password does not match!');
+    }
+
+}
+
 export default {
     register,
+    login,
+
 }
